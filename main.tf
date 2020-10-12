6f648e0bf79607fa144fe23f739ab98e1264a60a
Zbysek MRAZ <zmraz@adaptavist.com>
2020-10-12 15:10:07 +0100

    feat: adding labels module

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+module "labels" {
+  source    = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.5.0"
+  namespace = var.namespace
+  stage     = var.stage
+  name      = var.name
+  tags      = var.tags
+}
+
 module "aws-lambda" {
   source          = "Adaptavist/aws-lambda/module"
   version         = "1.4.0"
@@ -13,13 +21,13 @@ module "aws-lambda" {
   namespace = var.namespace
   name      = var.name
   stage     = var.stage
-  tags      = var.tags
+  tags      = module.labels.tags
 }
 
 resource "aws_sns_topic" "alarm" {
-  name            = "${var.function_name}-alarm-topic"
+  name            = "${module.labels.id}-${var.function_name}"
   delivery_policy = file("${path.module}/templates/aws_sns_topic.delivery_policy.json")
-  tags            = var.tags
+  tags            = module.labels.tags
 }
 
 resource "aws_sns_topic_subscription" "sns-alarm" {
